b70e9d357f5d75cc3377de186f3a6d10cd2db74b
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-02-01 16:26:41 -0500

    chore: Global store for fetching data once
    - Provides something similar to React Query (ie: `data`, `isLoading`, and `error`).

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./App.css";
+import { PetsStoreProvider } from "./services/PetsStore";
 import { Home } from "./pages/Home";
+
+import "./App.css";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <PetsStoreProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </PetsStoreProvider>
   );
 }
---
+++ src/services/PetsStore.tsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export type Pet = {
+  title: string;
+  description: string;
+  url: string;
+  created: string;
+};
+
+const ENDPOINT = "https://eulerity-hackathon.appspot.com/pets";
+
+type PetsStore = {
+  data: Pet[];
+  isLoading: boolean;
+  error?: Error;
+};
+
+const PetsStoreContext = createContext<PetsStore>(null as never);
+
+export function PetsStoreProvider(props: { children: React.ReactNode }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<Pet[]>([]);
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(ENDPOINT);
+      if (!res.ok) {
+        setError(new Error("Failed to fetch pets."));
+      } else {
+        const petsData = await res.json();
+        setData(petsData);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <PetsStoreContext.Provider value={{ data, isLoading, error }}>
+      {props.children}
+    </PetsStoreContext.Provider>
+  );
+}
+
+export function usePets() {
+  const store = useContext(PetsStoreContext);
+  if (!store) {
+    throw new Error("usePets must be called within a PetsStoreProvider.");
+  }
+  return store;
+}
